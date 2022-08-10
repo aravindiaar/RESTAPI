942911cbe9fb3161c885c444a612866837a8e1c3
Aravind <aravindiaar@gmail.com>
2022-08-10 22:52:26 +0530

    Recommit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+
+**RESTAPI C# .NET**
+
+https://localhost:44365/api/COntroller_Name
+
+**GET**
+https://localhost:44365/api/Process
+
+**PUT**
+https://localhost:44365/api/Process?ID=1
+
+**POST**
+https://localhost:44365/api/Process?ID=1
+
+**DELETE**
+https://localhost:44365/api/Process?ID=1
--- RESTAPI/Controllers/ProcessController.cs
+++ RESTAPI/Controllers/ProcessController.cs
@@ -28,19 +28,19 @@ namespace RESTAPI.Controllers
             //Deserializing to Object:
             var obj = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Model>>(jsonString);
 
-            return Request.CreateResponse(HttpStatusCode.OK, jsonString);
+            return Request.CreateResponse(HttpStatusCode.OK, ob);
         }
-        public HttpResponseMessage Put(int id)
+        public HttpResponseMessage Put(int ID)
         {
             return Request.CreateResponse(HttpStatusCode.OK, "Put Method");
         }
 
-        public HttpResponseMessage Post(string Data)
+        public HttpResponseMessage Post(int ID)
         {
             return Request.CreateResponse(HttpStatusCode.Created, "Post Method");
         }
 
-        public HttpResponseMessage Delete(int id)
+        public HttpResponseMessage Delete(int ID)
         {
             return Request.CreateResponse(HttpStatusCode.Accepted, "Delete Method");
         }
